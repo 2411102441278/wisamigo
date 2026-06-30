2279c4e5a6b70ee3300740767793b9384351771a
zulfiansyah <2411102441315@umkt.ac.id>
2026-06-30 23:08:27 +0800

    Use custom Wisamigo admin site in project URLs

--- wisamigo_project/urls.py
+++ wisamigo_project/urls.py
@@ -1,12 +1,16 @@
-from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 from django.views.generic import RedirectView
+try:
+    from travel.admin import admin_site
+except Exception:
+    from django.contrib import admin as admin_site
+    admin_site = admin_site.site
 
 urlpatterns = [
     path('admin/payment-dashboard/', RedirectView.as_view(url='/admin-dashboard/', permanent=False)),
-    path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
     path('', include('travel.urls')),
 ]
 
